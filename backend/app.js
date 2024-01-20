bdeb7bc9e465e73b31cd2eb6f15938431edf6120
Valentino Jaber <danial27@student.ubc.ca>
2024-01-20 14:38:17 -0800

    Draft Passage Login

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,17 @@ app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Define your API routes or other backend logic here
 
+// Route for signin page
+app.get('/signin', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/signin.html'));
+});
+
+// Route for signup page
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/signup.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
