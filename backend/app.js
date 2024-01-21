c316aeb793f123b5b9415d95a77a4d2db67384a2
Valentino Jaber <danial27@student.ubc.ca>
2024-01-20 16:31:07 -0800

    WIP Camera and Drug Scan

--- backend/app.js
+++ backend/app.js
@@ -23,3 +23,8 @@ app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
+// Route for signup page
+app.get('/camera', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/camera.html'));
+});
+
